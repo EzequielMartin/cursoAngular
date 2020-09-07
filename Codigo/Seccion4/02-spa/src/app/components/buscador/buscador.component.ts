3110123fb46aae6be342eeea71526f44fd81fd8b
Ezequiel Martin <ezequielmartin92@gmail.com>
2020-09-07 13:21:39 -0300

    Termine la seccion 4!

---
+++ Codigo/Seccion4/02-spa/src/app/components/buscador/buscador.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-buscador',
+  templateUrl: './buscador.component.html',
+})
+export class BuscadorComponent implements OnInit {
+
+  heroes: any[] = [];
+  termino: string;
+
+  constructor( private activatedRoute: ActivatedRoute,
+               private _heroesService: HeroesService,
+               private router: Router ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe( params => {
+      this.termino = params['termino'];
+      this.heroes = this._heroesService.buscarHeroes( params['termino'] );
+    });
+  }
+
+  verHeroe(idx: number): any{
+    this.router.navigate( ['/heroe', idx] );
+  }
+}
